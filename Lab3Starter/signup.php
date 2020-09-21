283b9acdc64a069db404a3e5f23c1f99f8b81e54
sohanpatel1221 <sohanpatel1221@gmail.com>
2020-09-20 23:08:08 -0400

    Sign Up and Logic

---
+++ Lab3Starter/signup.php
@@ -0,0 +1,72 @@
+<?php
+require "includes/header.php"
+?>
+
+<main>
+    <div class = "bg-cover">
+        <div class = "h-100 container center-me">
+            <div class="my-auto">
+                <div class="signup-form">
+                 <form action="includes/signup-helper.php" method="post">
+                 <h2> register </h2>
+                 <p class="hint-text"> create your account </p>
+                 <div class="form-group"> 
+                 <div class="row"> 
+                   
+                    <div class="col"> 
+                        <input type="text" class="form-control" name="fname" placeholder="first name" required="required">
+                    </div>
+
+                    <div class="col"> 
+                        <input type="text" class="form-control" name="lname" placeholder="last name" required="required">
+                    </div>
+
+
+                </div>
+                </div>
+                <div class="form-group"> 
+                 
+                        <input type="text" class="form-control" name="uname" placeholder="user name" required="required">
+             
+
+                </div>
+
+                <div class="form-group"> 
+                 
+                 <input type="email" class="form-control" name="email" placeholder="E-mail" required="required">
+      
+
+         </div>
+
+         <div class="form-group"> 
+                 
+                        <input type="password" class="form-control" name="pwd" placeholder="Password" required="required">
+             
+
+                </div>
+
+                <div class="form-group"> 
+                 
+                 <input type="password" class="form-control" name="con-pwd" placeholder="Confirm Password" required="required">
+      
+
+         </div>
+         <div class="form-group"> 
+                 
+                 <button type="submit" name="signup-submit" class="btn-outline-success btn-lg btn-block"> Register </button>
+      
+
+         </div>
+
+
+    </form>
+
+    <div class ="text-center"> Already a member <a href="login.html"> Sign in </a> </div>
+        
+</div>
+            </div>
+        </div>
+
+    </div>
+    
+</main>
