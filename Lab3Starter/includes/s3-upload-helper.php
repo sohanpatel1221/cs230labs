b4a6b8fd3c590f2eae041b293274bcdc008e354a
sohanpatel1221 <55931169+sohanpatel1221@users.noreply.github.com>
2020-11-11 21:28:38 -0500

    Update s3-upload-helper.php

--- Lab3Starter/includes/s3-upload-helper.php
+++ Lab3Starter/includes/s3-upload-helper.php
@@ -29,8 +29,8 @@ if(isset($_POST['s3-submit'])){
             array('region'=>'us-east-1',
             'version' => 'latest',
             'credentials' => array(
-                'key' => "AKIATKTRZ54DSZXPKZKE",
-                'secret' => "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"
+                'key' => "",
+                'secret' => ""
             )
             
             
